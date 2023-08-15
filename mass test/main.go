0d954e89a5186b292df7b242995255c22b8803d7
mthiaw <massaw6@gmail..com>
2023-08-15 00:58:03 +0000

    first try

---
+++ mass test/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Paths [][]string
+
+// -----sort's side functions---------//
+func (p *Paths) Len() int {
+	return len(*p)
+}
+
+func (p Paths) Less(i, j int) bool {
+	return len(p[i]) < len(p[j])
+}
+
+func (p Paths) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p *Paths) Sortpaths() {
+	sort.Sort(p)
+}
+
+// -------------------------------//
+func path_passer(pathtab Paths, antsize int) []int {
+	passing_ant := make([]int, pathtab.Len())
+	roomsize := make([]int, pathtab.Len())
+	for i := range roomsize {
+		roomsize[i] = len(pathtab[i])
+	}
+
+	index := 0
+	for i := 0; i < antsize; i++ {
+		roomspace := roomsize[index] + 1 + passing_ant[index]
+		if roomsize[index] != roomsize[len(roomsize)-1] {
+			if roomspace <= roomsize[index+1] {
+				passing_ant[index]++
+			} else {
+				passing_ant[index+1]++
+				index++
+			}
+		} else {
+			passing_ant[0]++
+
+		}
+	}
+
+
+
+	//removing the nil slices
+	for i, v := range passing_ant {
+		if v == 0 {
+			passing_ant = append(passing_ant[:i], passing_ant[i+1:]...)
+		}
+	}
+
+	return passing_ant
+}
+
+func Sequences(ants int, tab Paths) {
+	passers := path_passer(tab, ants)
+	fmt.Println("ants per path")
+	fmt.Println(passers)
+	initial := ants
+	for i := 1; initial >= 0; i++ {
+		for j := 0; j < tab.Len(); j++ {
+			if i < len(tab[j]) {
+				if j < len(tab[j]) {
+					step := fmt.Sprintf("L%v-L%s ", j+1, tab[j][i])
+					fmt.Print(step)
+				}
+			}
+		}
+		initial--
+		println()
+	}
+}
+
+func main() {
+	tab := Paths{
+		{"1", "2", "5", "6", "0"},
+		{"1", "3", "4", "0"},
+	}
+
+	fmt.Println("-----before sorting---------")
+	fmt.Println(tab)
+	fmt.Println("-----after sorting---------")
+	tab.Sortpaths()
+	fmt.Println(tab)
+	fmt.Println("-----sequences---------")
+	Sequences(3, tab)
+}
